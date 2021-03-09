43578d53d2c274cafe852c0f2b04a91f4b3e017c
💻 Eher <alexandre@eher.com.br>
2021-03-09 16:13:28 +0100

    $ go fmt main.go
    Beep Boop

--- main.go
+++ main.go
@@ -1,55 +1,56 @@
 package main
 
-import(
-    "bytes"
-    "fmt"
-    "html"
-    "io/ioutil"
-    "net/http"
-    "os"
+import (
+	"bytes"
+	"fmt"
+	"html"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-func postJson(url string, json []byte) (error) {
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
+func postJson(url string, json []byte) error {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
 
-    req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
 
-    client := &http.Client{}
+	client := &http.Client{}
 
-    resp, err := client.Do(req)
+	resp, err := client.Do(req)
 
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-    fmt.Println("response Body:", string(body))
+	fmt.Println("response Body:", string(body))
 
-    defer resp.Body.Close()
-    return err
+	defer resp.Body.Close()
+	return err
 }
 
 func defaultHandler(writer http.ResponseWriter, request *http.Request) {
-    body, _ := ioutil.ReadAll(request.Body)
-    fmt.Println("request Body:", string(body))
+	body, _ := ioutil.ReadAll(request.Body)
+	fmt.Println("request Body:", string(body))
 
-    var url = os.Getenv("WEBHOOK_URL")
-    var json = []byte(`{"text":"` + html.EscapeString(string(body)) + `"}`)
+	var url = os.Getenv("WEBHOOK_URL")
 
-    err := postJson(url, json)
-    if err != nil {
-        fmt.Fprintf(writer, "Error: %s", err.Error())
-        return
-    }
+	var json = []byte(`{"text":"` + html.EscapeString(string(body)) + `"}`)
 
-    fmt.Fprintf(writer, "OK")
+	err := postJson(url, json)
+	if err != nil {
+		fmt.Fprintf(writer, "Error: %s", err.Error())
+		return
+	}
+
+	fmt.Fprintf(writer, "OK")
 }
 
 func main() {
-    var server = fmt.Sprintf("0.0.0.0:%s", os.Getenv("HTTP_PORT"))
+	var server = fmt.Sprintf("0.0.0.0:%s", os.Getenv("HTTP_PORT"))
 
-    fmt.Println("Server listening on", server)
+	fmt.Println("Server listening on", server)
 
-    http.HandleFunc("/", defaultHandler)
-    http.ListenAndServe(server, nil)
+	http.HandleFunc("/", defaultHandler)
+	http.ListenAndServe(server, nil)
 }
