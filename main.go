0250e0700099ddf03954e5808b7362dc266dec56
💻 Eher <alexandre@eher.com.br>
2021-03-11 23:12:49 +0100

    $ go fmt
    Beep Boop!
    
    Co-authored-by: Jefersson Nathan <malukenho.dev@gmail.com>
    Signed-Off-By: Alexandre Eher <alexandre.eher@werkspot.nl>

--- main.go
+++ main.go
@@ -16,9 +16,11 @@ func assert(e error) {
 // HTTPClient interface
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
-} 
+}
 
-var (Client HTTPClient)
+var (
+	Client HTTPClient
+)
 
 func init() {
 	Client = &http.Client{}
