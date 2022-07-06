6761930cb2070b4540079deadb3e7fa6b0ab8399
Jefersson Nathan <malukenho.dev@gmail.com>
2022-07-06 13:50:46 +0200

    s/assert.Empty/assert.Nil/g
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- provider/slack_test.go
+++ provider/slack_test.go
@@ -17,15 +17,15 @@ func TestChannelsForMergeRequestSingleRule(t *testing.T) {
 	var reactions []model.ReactionRule
 
 	payload, err := ioutil.ReadFile("../payload/merge_request-open-just-testing.json")
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	err = json.Unmarshal(payload, &mergeRequest)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "just-testing", mergeRequest.Labels[0].Title)
 
 	jsonString = "[{\"channel\":\"#tested\",\"labels\":[\"just-testing\"]}]"
 	err = json.Unmarshal([]byte(jsonString), &notifications)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	jsonString = "[{\"action\":\"approved\",\"reaction\":\"thumbsup\"}]"
 	err = json.Unmarshal([]byte(jsonString), &reactions)
@@ -52,15 +52,15 @@ func TestChannelsForMergeRequestMultipleRules(t *testing.T) {
 	var reactions []model.ReactionRule
 
 	payload, err := ioutil.ReadFile("../payload/merge_request-open-enabling-team.json")
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	err = json.Unmarshal(payload, &mergeRequest)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "Enabling Team", mergeRequest.Labels[0].Title)
 
 	jsonString = "[{\"channel\":\"#tested\",\"labels\":[\"just-testing\"]},{\"channel\":\"#multiple-rules\",\"labels\":[\"Enabling Team\"]}]"
 	err = json.Unmarshal([]byte(jsonString), &notifications)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	jsonString = "[{\"action\":\"approved\",\"reaction\":\"thumbsup\"}]"
 	err = json.Unmarshal([]byte(jsonString), &reactions)
@@ -87,15 +87,15 @@ func TestChannelsForMergeRequestMultipleRulesWithMoreThanOneLabel(t *testing.T) 
 	var reactions []model.ReactionRule
 
 	payload, err := ioutil.ReadFile("../payload/merge_request-open-enabling-team.json")
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	err = json.Unmarshal(payload, &mergeRequest)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "Enabling Team", mergeRequest.Labels[0].Title)
 
 	jsonString = "[{\"channel\":\"#tested\",\"labels\":[\"just-testing\", \"find-my-bug\"]},{\"channel\":\"#multiple-rules\",\"labels\":[\"Team Magie Lee\", \"Enabling Team\"]}]"
 	err = json.Unmarshal([]byte(jsonString), &notifications)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	jsonString = "[{\"action\":\"approved\",\"reaction\":\"thumbsup\"}]"
 	err = json.Unmarshal([]byte(jsonString), &reactions)
@@ -122,19 +122,19 @@ func TestChannelsForMergeRequestNotMatchingLabel(t *testing.T) {
 	var reactions []model.ReactionRule
 
 	payload, err := ioutil.ReadFile("../payload/merge_request-open-just-testing.json")
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	err = json.Unmarshal(payload, &mergeRequest)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "just-testing", mergeRequest.Labels[0].Title)
 
 	jsonString = "[{\"channel\":\"#tested\",\"labels\":[\"something-else\"]}]"
 	err = json.Unmarshal([]byte(jsonString), &notifications)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	jsonString = "[{\"action\":\"approved\",\"reaction\":\"thumbsup\"}]"
 	err = json.Unmarshal([]byte(jsonString), &reactions)
-	assert.Empty(t, err)
+	assert.Nil(t, err)
 
 	slack := NewSlack(
 		http.DefaultClient,
