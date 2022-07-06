e2f5bb13c69b24d6e63c02d7b03c75a7e414b9f7
Jefersson Nathan <malukenho.dev@gmail.com>
2022-07-06 13:18:03 +0200

    add tests to new env var requirements
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- main_test.go
+++ main_test.go
@@ -9,9 +9,11 @@ func setupTestEnvironmentVariables() {
 	os.Setenv("HTTP_PORT", "Just testing...")
 	os.Setenv("MESSAGE", "Just testing...")
 	os.Setenv("NOTIFICATION_RULES", "[{\"channel\":\"#test\", \"labels\":[\"test\"]}]")
+	os.Setenv("REACTION_RULES", "[{\"action\":\"approved\", \"reaction\":\"thumbsup\"}]")
 	os.Setenv("SLACK_AVATAR_URL", "Just testing...")
 	os.Setenv("SLACK_USERNAME", "Just testing...")
 	os.Setenv("SLACK_WEBHOOK_URL", "Just testing...")
+	os.Setenv("SLACK_TOKEN", "xoxp-slack-token")
 }
 
 func TestEnvironmentMissingHttpPort(t *testing.T) {
@@ -47,6 +49,17 @@ func TestEnvironmentMissingNotificationRules(t *testing.T) {
 	t.Error("should panic about missing env: NOTIFICATION_RULES")
 }
 
+func TestEnvironmentMissingReactionRules(t *testing.T) {
+	defer func() { recover() }()
+
+	setupTestEnvironmentVariables()
+	os.Unsetenv("REACTION_RULES")
+
+	InitEnvironment()
+
+	t.Error("should panic about missing env: REACTION_RULES")
+}
+
 func TestEnvironmentMissingSlackAvatarUrl(t *testing.T) {
 	defer func() { recover() }()
 
@@ -78,4 +91,15 @@ func TestEnvironmentMissingSlackWebhookUrl(t *testing.T) {
 	InitEnvironment()
 
 	t.Error("should panic about missing env: SLACK_WEBHOOK_URL")
+}
+
+func TestEnvironmentMissingSlackToken(t *testing.T) {
+	defer func() { recover() }()
+
+	setupTestEnvironmentVariables()
+	os.Unsetenv("SLACK_TOKEN")
+
+	InitEnvironment()
+
+	t.Error("should panic about missing env: SLACK_TOKEN")
 }
