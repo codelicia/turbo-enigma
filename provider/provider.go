b7919bde030211db80bc62d6fa96607f0e7fbcdb
Jefersson Nathan <malukenho.dev@gmail.com>
2022-07-07 17:15:39 +0200

    Add missing methods to the provider interface
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- provider/provider.go
+++ provider/provider.go
@@ -6,6 +6,12 @@ import (
 
 type Provider interface {
 	NotifyMergeRequestOpened(mergeRequest model.MergeRequestInfo) error
-	NotifyMergeRequestMerged(mergeRequest model.MergeRequestInfo) error
 	NotifyMergeRequestApproved(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestUnapproved(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestClose(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestReopen(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestUpdate(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestApproval(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestUnapproval(mergeRequest model.MergeRequestInfo) error
+	NotifyMergeRequestMerged(mergeRequest model.MergeRequestInfo) error
 }
